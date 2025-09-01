cdaca15e10f25c2d37d0bff643fba443d484fd23
Gijs Straathof <gbstraathof@gmail.com>
2025-09-01 22:17:17 +0200

    sleep task check and update config upon upgrade

--- scripts/update_birdnet_snippets.sh
+++ scripts/update_birdnet_snippets.sh
@@ -118,6 +118,10 @@ if ! grep -E '^AUTOMATIC_UPDATE=' /etc/birdnet/birdnet.conf &>/dev/null;then
   echo "AUTOMATIC_UPDATE=0" >> /etc/birdnet/birdnet.conf
 fi
 
+if ! grep -E '^LUISTERVINK_ENABLE_TASK_PROCESSOR=' /etc/birdnet/birdnet.conf &>/dev/null;then
+  echo "LUISTERVINK_ENABLE_TASK_PROCESSOR=true" >> /etc/birdnet/birdnet.conf
+fi
+
 if ! grep -E '^RARE_SPECIES_THRESHOLD=' /etc/birdnet/birdnet.conf &>/dev/null;then
   echo '## RARE_SPECIES_THRESHOLD defines after how many days a species is considered as rare and highlighted on overview page' >> /etc/birdnet/birdnet.conf
   echo "RARE_SPECIES_THRESHOLD=\"30\"" >> /etc/birdnet/birdnet.conf
